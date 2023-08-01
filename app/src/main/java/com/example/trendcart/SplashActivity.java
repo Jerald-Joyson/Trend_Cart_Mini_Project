e4110a446d8d5f2b76d7f822a50a76815fcfa121
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2023-08-01 14:09:38 +0530

    wellcome screen and navigation is set

--- app/src/main/java/com/example/trendcart/SplashActivity.java
+++ app/src/main/java/com/example/trendcart/SplashActivity.java
@@ -12,12 +12,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
-            }
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, welcomeActivity1.class));
+            finish();
         }, 3000);
     }
 }
---
+++ app/src/main/java/com/example/trendcart/welcomeActivity2.java
@@ -0,0 +1,25 @@
+package com.example.trendcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class welcomeActivity2 extends AppCompatActivity {
+    Button nextBtn;
+    ImageButton backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome2);
+        nextBtn = findViewById(R.id.nextbtn);
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(v -> onBackPressed());
+        nextBtn.setOnClickListener(v -> startActivity(new Intent(welcomeActivity2.this, welcomeActivity3.class)));
+    }
+}
